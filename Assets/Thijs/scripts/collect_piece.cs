448d5bcd1c7f2fc60b32b20d99c98e9c4f75833f
gingerappa <t.swinkels@sintlucasedu.nl>
2024-02-07 12:23:30 +0100

    added particles

--- Assets/Thijs/scripts/collect_piece.cs
+++ Assets/Thijs/scripts/collect_piece.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class collect_piece : MonoBehaviour
 {
     public static int piecesCollected;
+    public GameObject particlePrefab;
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "piece")
+        if (gameObject.tag == "piece" && collision.gameObject.tag == "player")
         {
             piecesCollected++;
             Debug.Log(piecesCollected);
-            Destroy(collision.gameObject);
+            GameObject particle = Instantiate(particlePrefab, gameObject.transform.position, Quaternion.identity);
+            particle.GetComponent<ParticleSystem>().Play();
+            Destroy(particle, 1);
+            Destroy(gameObject);
         }
     }
 }
